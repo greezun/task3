680f35f9833ea7285384879baf6d1fb689a7c8a0
Yevhenii Maslak <greezun@gmail.com>
2023-03-30 20:26:19 +0300

    deleted dialogFragment

--- app/src/main/java/shpp/maslak/task3/ui/ContactActivity.kt
+++ app/src/main/java/shpp/maslak/task3/ui/ContactActivity.kt
@@ -1,18 +1,15 @@
 package shpp.maslak.task3.ui
 
 import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import shpp.maslak.task3.R
 import shpp.maslak.task3.databinding.ActivityContactBinding
 import shpp.maslak.task3.ui.base.BaseActivity
 
-class ContactActivity : BaseActivity<ActivityContactBinding>(ActivityContactBinding::inflate){
+class ContactActivity : BaseActivity<ActivityContactBinding>(ActivityContactBinding::inflate)
+{
 
     private lateinit var navController: NavController
 
@@ -29,6 +26,8 @@ class ContactActivity : BaseActivity<ActivityContactBinding>(ActivityContactBind
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+
 
 
 }
--- app/src/main/java/shpp/maslak/task3/ui/fragments/myContact/FragmentMyContacts.kt
+++ app/src/main/java/shpp/maslak/task3/ui/fragments/myContact/FragmentMyContacts.kt
@@ -2,15 +2,12 @@ package shpp.maslak.task3.ui.fragments.myContact
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,13 +20,11 @@ import shpp.maslak.task3.ui.base.BaseFragment
 import shpp.maslak.task3.util.model.Contact
 
 
-class FragmentMyContacts: BaseFragment<FragmentMyContactsBinding>(FragmentMyContactsBinding::inflate)
-    {
+class FragmentMyContacts :
+    BaseFragment<FragmentMyContactsBinding>(FragmentMyContactsBinding::inflate) {
     private lateinit var addContact: AppCompatTextView
     private val adapter: ContactAdapter by lazy { createAdapter() }
     private val contactViewModel: ViewModelForContacts by viewModels()
-
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,15 +35,12 @@ class FragmentMyContacts: BaseFragment<FragmentMyContactsBinding>(FragmentMyCont
     }
 
 
-
     override fun setListeners() {
-        onClickAddContactListener()
         onSwipeToDeleteListener()
     }
 
 
-
-        private fun bindFields() {
+    private fun bindFields() {
         val manager = LinearLayoutManager(requireContext())
         with(binding) {
             addContact = tvAddContact
@@ -77,29 +69,18 @@ class FragmentMyContacts: BaseFragment<FragmentMyContactsBinding>(FragmentMyCont
 
     }
 
-    private fun onClickAddContactListener() {
-        binding.tvAddContact.setOnClickListener {
-            val contactAdder = DialogAddContact()
-            contactAdder.show(parentFragmentManager, DialogAddContact.TAG)
-        }
-    }
 
     override fun setObservers() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 contactViewModel.contactState.collect { list ->
                     adapter.submitList(list)
-                    Log.d("myLog","observ ${list.toString()}")
+                    Log.d("myLog", "observ ${list.toString()}")
                 }
             }
         }
     }
 
-//    override fun onConfirmButtonClicked(contact: Contact) {
-//        contactViewModel.addContact(contact)
-//        Snackbar.make(binding.root, MESSAGE_ADD_CONTACT , Snackbar.LENGTH_SHORT)
-//            .show()
-//    }
 
     private fun createAdapter(): ContactAdapter {
         return ContactAdapter(contactActionListener = object : ContactActionListener {
@@ -117,31 +98,16 @@ class FragmentMyContacts: BaseFragment<FragmentMyContactsBinding>(FragmentMyCont
     }
 
     private fun showDeleteMessage(index: Int, contact: Contact) {
-        Snackbar.make(binding.root, MESSAGE_DELETE , Snackbar.LENGTH_LONG)
+        Snackbar.make(binding.root, MESSAGE_DELETE, Snackbar.LENGTH_LONG)
             .setAction(SNACKBAR_ACTION_BUTTON_TEXT) {
                 contactViewModel.addContactOnIndex(index, contact)
             }
             .show()
     }
-    companion object{
-        private const val MESSAGE_ADD_CONTACT = "Contact added"
+
+    companion object {
         private const val MESSAGE_DELETE = "Contact has been deleted"
         private const val SNACKBAR_ACTION_BUTTON_TEXT = "UNDO"
-
-        @JvmStatic private val KEY_LABEL = "KEY_LABEL"
-
-
-
-        @JvmStatic
-        fun getInstance(toolbarLabel: Int): FragmentMyContacts {
-            val args: Bundle = Bundle().apply {
-                putInt(KEY_LABEL, toolbarLabel)
-            }
-            val fragment = FragmentMyContacts()
-            fragment.arguments = args
-
-            return fragment
-        }
     }
 
 
